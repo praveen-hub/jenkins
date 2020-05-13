4973b268163ee84f5ac86629c2b6c8b40f4c7912
praveen <praveen@gmail.com>
2020-05-13 12:48:26 +0530

    modified git test

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ class Test {
     public static void main(String args[]){
          for(int i=10; i>1; i--){
               System.out.println("The value of i is: "+i);
+              System.out.println("My name is praveen kumar, working in tcs");
          }
     }
 }
